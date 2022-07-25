03256d5be28c44fae9329baddef4fc33769c97ea
KYapp <kukiyomenaisapp@gmail.com>
2022-07-26 00:07:02 +0900

    2

--- test3/test3.go
+++ test3/test3.go
@@ -5,4 +5,11 @@ import "fmt"
 func main() {
 	var ns = [5]int{10, 20, 30, 40, 50}
 	fmt.Println(ns)
+
+	ns2 := [...]int{5: 50, 100: 100}
+	fmt.Println(ns2)
+
+	println(ns2[3])
+	println(len(ns))
+	fmt.Println(ns[1:2])
 }
